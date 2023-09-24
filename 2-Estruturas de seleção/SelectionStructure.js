e723fedac5baaef4b06d4c495617dc8fbb76290e
Emerson-Oliver <emerson.520@outlook.com>
2023-09-24 13:33:22 -0300

    Aprendendo regras, sintaxe

---
+++ 2-Estruturas de seleção/SelectionStructure.js
@@ -0,0 +1,10 @@
+// if (<condição>) {...}
+// -> False: 0, undefined, null, " ", NaN
+
+const temperatura = 40;
+
+if(temperatura > 25){
+    console.log("Esta Calor!");
+    console.log("A tempperatura é: " + temperatura);
+}
+console.log("Fim");
