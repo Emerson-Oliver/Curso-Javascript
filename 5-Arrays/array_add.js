70dead016117cf1ab8db019a689dbe8a3bef57b5
Emerson-Oliver <emerson.520@outlook.com>
2023-09-27 17:04:54 -0300

    Aprendendo adicionar valores dentro do array

---
+++ 5-Arrays/array_add.js
@@ -0,0 +1,6 @@
+const frutas = ["Morango", "Manga", "Carambola", "Abacate"];
+
+frutas.push("Uva", "Maça"); // Adiciona valor no final do array
+frutas.unshift("Abacaxi"); // Adiciona valor no inicio do array
+
+console.log(frutas.toString()); //.toString converte o array para uma representação em string
