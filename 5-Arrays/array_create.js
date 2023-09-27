eec2085ce6d115e4f5547c774a5d1aecae8a963d
Emerson-Oliver <emerson.520@outlook.com>
2023-09-27 16:17:33 -0300

    Aprendendo Arrays

---
+++ 5-Arrays/array_create.js
@@ -0,0 +1,9 @@
+const meuArray = [5, 7 , 5, 3, 6];
+
+console.log(meuArray); // Mostra toso os indices do array
+console.log(meuArray[2]); // Mostra o valor no indice 2 dentro do array no cado é o valor 5
+console.log(meuArray.length); //Mostra tamanho do array
+
+meuArray[0] = 38; // Substitui o indice 0 "5" por 10
+
+console.log(meuArray);
